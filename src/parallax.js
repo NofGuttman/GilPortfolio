542d7a8353fc61371d74ab1786c6a89dab34d405
Nof Guttman <nofguttman@gmail.com>
2017-11-10 02:13:16 +0200

    new parallax settings

--- src/parallax.js
+++ src/parallax.js
@@ -1,9 +1,13 @@
+var randomListOfScrolls = [];
+
+for(let i = 0; i < 12; i++) {
+  randomListOfScrolls.push([Math.random() * 8 - 1, Math.random() * 2 - 1]);
+}
+
 $(document).scroll(function() {
   let scroll = $(this).scrollTop();
-  
-  let listOfScroll = [4, 3, 0, 5, 2, 6, 4, 3, 7, 4, -2, 1];
-  
+    
   $(".header").children("span").each(function(i) {
-    $(this).css({ transform: "translateY(" + scroll * (listOfScroll[i] / 10) + "px)"})
+    $(this).css({ transform: "translate(" + scroll * (randomListOfScrolls[i][1] / 10) + "px, " + scroll * (randomListOfScrolls[i][0] / 10) + "px)"});
   });
 });
